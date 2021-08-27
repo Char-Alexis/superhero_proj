257c9996b005e27227d778f62ff5539f0cf17d1e
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-08-26 23:24:31 -0400

    Created delete function on views.py

---
+++ superhero_project/superheroes/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Superhero
+
+# Create your views here.
+def index(request):
+    all_heroes = Superhero.objects.all()
+    context = {
+        'all_heroes': all_heroes
+    }
+    return render(request, 'superheroes/index.html', context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        # name.delete()
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero= Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+
+def delete(request, hero_id):
+    delete_hero= Superhero.objects.get(hero_id)
+    delete_hero.delete()
+    return redirect('index')
